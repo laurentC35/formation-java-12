4c06e2c9be3b3a2215ade18c986887bf1f269c8e
Laurent Caouissin <laurent.caouissin@insee.fr>
2020-01-09 12:01:57 +0100

    etape2+3

--- Part1/src/exo1/Etape1.java
+++ Part1/src/exo1/Etape1.java
@@ -1,5 +1,33 @@
 package exo1;
 
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Etape1 {
+	
+	protected static Runnable runnable = () -> {System.out.println("Exo1.Etape1");};
+	
+	public static void main(String[] args) {
+	
+		String[] students = {"Ludovic","Laurent","Fabrice","Mathieu","Olivier","Benjamin","Betty","Loïc","Nicolas","Simon"};
+		
+		/**
+		 * But de la classe : crée un poll de thread,
+		 * On peut le remplir de runnable, on les execute par paquet
+		 * une fois shutdown, on ne peut plus rien faire
+		 */
+		ExecutorService service = Executors.newSingleThreadExecutor();
+		service.submit(runnable);
+		service.shutdown();
+		
+		System.out.println("Non Triée");
+		Arrays.asList(students).forEach(System.out::println);
+		
+		Arrays.sort(students, (student1,student2) -> student1.compareTo(student2));
+
+		System.out.println("Triée");
+		Arrays.asList(students).forEach(System.out::println);
+	}
 
 }
